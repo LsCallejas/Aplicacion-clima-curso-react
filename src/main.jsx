5a2461d2f5a8190f730793352fa1b25cb30d9746
LsCallejas <lscallejas14@gmail.com>
2024-03-22 10:04:33 -0400

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { WheatherApp } from './WheatherApp'
+import './style/weatherStyles.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <WheatherApp/> 
+  </React.StrictMode>,
+)
+//7fb04debcb84e50252a8471d94db72f9 
